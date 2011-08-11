d6cdefe51f0abc54d9a62c5031b1ce6382cbfbe8
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-08-11 00:30:27 +0000

    Se crea estructura de componente YuppGis. Se agregaron tipos de datos geograficos basicos.

---
+++ trunk/yuppgis/core/basic/yuppgis.core.basic.Point.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class Point extends Geometry {
+	
+	private $x;
+	private $y;
+	
+	public function __construct($x, $y){
+		$this->x = $x;
+		$this->y = $y;
+	}
+	
+	public function getX() {
+		return $this->x;
+	}
+	
+	public function getY() {
+		return $this->y;
+	}
+	
+	public function setY($y) {
+		$this->y = $y;
+	}
+	
+	public function setX($x) {
+		$this->x = $x;
+	}
+	
+}
+
+?>
