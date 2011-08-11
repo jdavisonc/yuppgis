933af93045ebaea447ee5b3fc8aa80225bd31357
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-08-11 00:30:27 +0000

    Se crea estructura de componente YuppGis. Se agregaron tipos de datos geograficos basicos.

---
+++ yuppgis/core/db/yuppgis.core.db.GISDatatypes.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class GISDatatypes {
+	
+	const POINT = "yuppgis_type_point";
+	const LINESTRING = "yuppgis_type_linestring";
+	const LINE = "yuppgis_type_line";
+	const LINERING = "yuppgis_type_linering";
+	const POLYGON = "yuppgis_type_polygon";
+	
+}
+
+?>
