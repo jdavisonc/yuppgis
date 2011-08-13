c24cd8aae40233916e486c0060c32e184dfa7b95
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-08-13 19:47:27 +0000

    Agrego attrs a paciente.

--- apps/prototipo/model/prototipo.model.Paciente.class.php
+++ apps/prototipo/model/prototipo.model.Paciente.class.php
@@ -1,7 +1,27 @@
 <?php
 
-class Paciente{
-	
+class Paciente extends PersistentObject {
+
+	function __construct($args = array (), $isSimpleInstance = false) {
+		
+		$this->setWithTable("prototipo_paciente");
+
+		$this->addAttribute("nombre", Datatypes :: TEXT);
+
+	 	// Restricciones
+		$this->addConstraints("nombre", array(
+			Constraint::nullable(false),
+			Constraint::blank(false)
+		));
+
+		parent :: __construct($args, $isSimpleInstance);
+	}
+
+	public static function listAll(ArrayObject $params) {
+		self :: $thisClass = __CLASS__;
+		return PersistentObject :: listAll($params);
+	}
+
 }
 
 ?>
