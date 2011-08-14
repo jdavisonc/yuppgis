9eddc9f75794ce2651d514335e64f6d94ae9027c
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-08-14 22:19:54 +0000

    Agrego extension de GisPersistentObject en Paciente.

--- trunk/yuppgis/core/persistent/yuppgis.core.persistent.GISPersistentObject.class.php
+++ trunk/yuppgis/core/persistent/yuppgis.core.persistent.GISPersistentObject.class.php
@@ -1,8 +1,29 @@
 <?php
 
+// Se importa datatypes geograficos
+YuppLoader :: load('yuppgis.core.db', 'GISDatatypes');
+
+// Se importa modelo de datos geograficos
+YuppLoader :: load('yuppgis.core.basic', 'Geometry');
+YuppLoader :: load('yuppgis.core.basic', 'Point');
 
 class GISPersistentObject extends PersistentObject {
 	
+	public function addAttribute($name, $type) {
+		
+		switch($type) {
+		    case GISDatatypes::POINT:
+		    case GISDatatypes::LINE:
+		    case GISDatatypes::LINERING:
+		    case GISDatatypes::LINESTRING:
+		    case GISDatatypes::POLYGON:
+		        parent::addHasOne($name, Point::getClassName());
+		    	break;
+		    default;
+		        parent::addAttribute($name, $type);
+		    break;
+		}
+	}
 	
 	
 }
