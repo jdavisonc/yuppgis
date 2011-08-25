3af79ea5238d75de2751069ead540df170b0558a
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-08-25 19:28:15 +0000

    Agrego funciones complementarias de assert

--- trunk/yuppgis/core/testing/yuppgis.core.testing.YuppGISTestCase.class.php
+++ trunk/yuppgis/core/testing/yuppgis.core.testing.YuppGISTestCase.class.php
@@ -5,23 +5,49 @@ YuppLoader::load('core.testing', 'TestCase');
 YuppLoader::load('yuppgis.core.utils', 'ReflectionUtils');
 
 /**
- * Clase para facilitar los test unitarios, por defecto ejecuta todos los metodos que 
+ * Clase que facilita los test unitarios, por defecto ejecuta todos los metodos que 
  * cuyo nombre comience con la palabra 'test'.
+ * Ej: testPersistencia();
  *  
  * @author harley
  */
 abstract class YuppGISTestCase extends TestCase {
 	
 	public function run() {
-		
-		// Obtengo metodos de clase
 		$tests = ReflectionUtils::ReflectMethods(get_called_class(), 'test', false);
 		
 		// Ejecuto metodos obtenidos por reflection
 		foreach ( $tests as $test ) {
 			$this->$test();
 		}
-		
+	}
+	
+	public function assertEquals($expected, $actual, $msg) {
+		$this->assert($expected === $actual, $msg);
+	}
+	
+	public function assertNotEquals($expected, $actual, $msg) {
+		$this->assert($expected !== $actual, $msg);
+	}
+	
+	public function assertNull($actual, $msg) {
+		$this->assert($actual == null, $msg);
+	}
+	
+	public function assertNotNull($actual, $msg) {
+		$this->assert($actual != null, $msg);
+	}
+	
+	public function assertXMLEquals($xmlExpected, $xmlActual, $msg) {
+		$expected = new DOMDocument;
+        $expected->preserveWhiteSpace = FALSE;
+        $expected->loadXML($xmlExpected);
+
+        $actual = new DOMDocument;
+        $actual->preserveWhiteSpace = FALSE;
+        $actual->loadXML($xmlActual);
+        
+        $this->assert($expected == $actual, $msg);
 	}
 	
 }
