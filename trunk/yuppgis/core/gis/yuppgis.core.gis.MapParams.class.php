9e76bc6825d046a80662f3931af48c6c96732edf
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-09-11 15:26:27 +0000

    Agrego google maps key configurable.

--- trunk/yuppgis/core/gis/yuppgis.core.gis.MapParams.class.php
+++ trunk/yuppgis/core/gis/yuppgis.core.gis.MapParams.class.php
@@ -1,5 +1,9 @@
 <?php
+
+YuppLoader :: load('yuppgis.core.config', 'YuppGISConfig');
+
 class MapParams{
+	
 	const ID = "Id";
 	const OpenLayerJS_URL = "OLUrl";
 	const WIDTH = "Width";
@@ -12,11 +16,14 @@ class MapParams{
 		if($array != null && array_key_exists ( $key , $array ))			
 			return $array[$key];
 		else{
+			$appName = YuppContext::getInstance()->getApp();
+			$gmaps_key = YuppGISConfig::getInstance()->getGISPropertyValue($appName, YuppGISConfig::PROP_GOOGLE_MAPS_KEY);
+			
 			switch ($key){
 				case MapParams::ID:
 					return -1;
 				case MapParams::OpenLayerJS_URL:
-					return "http://maps.google.com/maps?file=api&v=2&key=your_key_here";
+					return "http://maps.google.com/maps?file=api&v=2&key=". $gmaps_key;
 				case MapParams::WIDTH:
 					return "500px";
 				case MapParams::HEIGHT:
