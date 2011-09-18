dfd48b7712a5c7cd865c7d8dba4a350117a493ac
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-09-18 00:54:58 +0000

    Cambio callback de uiproperty por prevalidate.

--- trunk/yuppgis/core/basic/yuppgis.core.basic.Geometry.class.php
+++ trunk/yuppgis/core/basic/yuppgis.core.basic.Geometry.class.php
@@ -5,13 +5,9 @@ YuppLoader::load('yuppgis.core.basic.ui', 'UIProperty');
 class Geometry extends GISPersistentObject {
 	
 	private $uiPropertyObject = null;
-	private $callbackUpdate = null;
 	
 	public function __construct($args = array (), $isSimpleInstance = false) {
 		$this->addAttribute('uiproperty', Datatypes::TEXT);
-
-		$this->callbackUpdate = new Callback();
-		$this->callbackUpdate->set( $this, 'serializeUIProperty', array() );
 		
 		parent :: __construct($args, $isSimpleInstance);
 	}
@@ -25,7 +21,6 @@ class Geometry extends GISPersistentObject {
 	}
 	
 	public function getUIProperty() {
-		$this->registerToUpdateUIProperty();
 		if ($this->uiPropertyObject == null) {
 			$this->uiPropertyObject = UIProperty::fromJSON($this->aGet('uiproperty'));
 		}
@@ -33,18 +28,15 @@ class Geometry extends GISPersistentObject {
 	}
 	
 	public function setUIProperty($uiProperty) {
-		$this->registerToUpdateUIProperty();
 		$this->uiPropertyObject = $uiProperty;
 	}
 	
-	public function serializeUIProperty() {
+	/**
+	 * Se serializa el atributo uiproperty a JSON
+	 * @see PersistentObject::preValidate()
+	 */
+	public function preValidate(){
 		$this->aSet('uiproperty', UIProperty::toJSON($this->uiPropertyObject));
-	}
-	
-	private function registerToUpdateUIProperty() {
-		if ($this->isDirty()) {
-			$this->registerBeforeSaveCallback($this->callbackUpdate);
-		}
 	}
 
 }
