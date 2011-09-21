bf8060a27a12806cced924b583f081085c5b7fc9
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-09-21 00:59:34 +0000

    Agrego filtros de igualdad, interseccion.

--- apps/prototipo/tests/apps.prototipo.tests.GISPersistentObjectTest.class.php
+++ apps/prototipo/tests/apps.prototipo.tests.GISPersistentObjectTest.class.php
@@ -86,7 +86,30 @@ class GISPersistentObjectTest extends YuppGISTestCase {
 		$pacientes = Paciente::findBy($and, new ArrayObject());
 		
 		$this->assert($pacientes !== null, 'Test de filtrado de pacientes');
+	}
+	
+	function testFindByEquals() {
+		$and = Condition::_AND()
+			->add(GISCondition::EQGEO(
+				YuppGISConventions::tableName(Paciente::getClassName()), 
+				'ubicacion', new Point(10, 10)))
+			->add(Condition::EQ(YuppGISConventions::tableName(Paciente::getClassName()), 'nombre', 'Roberto'));
 		
+		$pacientes = Paciente::findBy($and, new ArrayObject());
+		
+		$this->assert($pacientes !== null, 'Test de filtrado de pacientes por igualdad de figuras');
+	}
+	
+	function testFindByIntersection() {
+		$and = Condition::_AND()
+			->add(GISCondition::INTERSECTS(
+				YuppGISConventions::tableName(Paciente::getClassName()), 
+				'ubicacion', new Point(10, 10)))
+			->add(Condition::EQ(YuppGISConventions::tableName(Paciente::getClassName()), 'nombre', 'Roberto'));
+		
+		$pacientes = Paciente::findBy($and, new ArrayObject());
+		
+		$this->assert($pacientes !== null, 'Test de filtrado de pacientes por interseccion');
 	}
 	
 	public function testJSON2() {
