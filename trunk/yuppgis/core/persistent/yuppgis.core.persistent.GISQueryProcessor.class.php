17f03b36ff8cfe1180589127f0a68bb82db17fa2
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-03 22:23:05 +0000

    Beta de GISQUeryProcessor

--- trunk/yuppgis/core/persistent/yuppgis.core.persistent.GISQueryProcessor.class.php
+++ trunk/yuppgis/core/persistent/yuppgis.core.persistent.GISQueryProcessor.class.php
@@ -2,31 +2,96 @@
 
 class GISQueryProcessor {
 	
-	public function process() {
+	public function process(Query $query) {
+		
+		//0. Obtengo atributos geograficos posibles en la query
+		$geoAttrsOfQuery = self::getGeometryAttrs($query->getFrom());
 		
 		//1. Armamos las dos bolsas de Selects
-		self::processSelects();
+		$processedSelects = self::processSelects($query->getSelect(), $geoAttrsOfQuery);
 		
 		//2. Se ejecuta query sobre DB (no Geo)
-		self::executeSimpleQuery();
+		$mainResult = self::executeSimpleQuery($query, $processedSelects->mainSelect);
 		
 		//3. Se arman las GISQuery con los res de 1 y 2 
-		self::processGISQuerys();
+		$gisQueries = self::createGISQuerys($query->getFrom(), $processedSelects->gisSelect, $mainResult);
 		
 		//4. 
-		self::executeGISQuerys();
+		$gisResults = self::executeGISQuerys($gisQueries);
 		
 		//5. Se arma resultado final
-		self::processResult();
+		$finalResult = self::processResults($mainResult, $gisResults);
 		
+		return $finalResult;
 	}
 	
 	/**
 	 * A partir de los Selects y Froms originales genera los Selects para ir contra la base no geografica
-	 * y los selects para ir contra la base geo
+	 * y los selects para ir contra la base geo.
+	 * 
+	 * @param Select	select de la consulta a procesar
+	 * @param array		atributos geograficos posibles de la consulta
+	 * @return ProcessedSelects		 objeto con select principal y geograficos
 	 */
-	private static function processSelects() {
-		//TODO_GIS
+	private static function processSelects(Select $select, $geoAttrsOfQuery) {
+		$processedSelects = new ProcessedSelects();
+
+		foreach ($select->getAll() as $selectItem) {
+			self::GISProjectionToProjection($geoAttrsOfQuery, $selectItem, $processedSelects);
+		}
+		
+		return $processedSelects;
+	}
+	
+	private static function GISProjectionToProjection($geoAttrsOfQuery, $selectItem, ProcessedSelects $processedSelects) {
+		if ($selectItem instanceof SelectAttribute) {
+			$alias = $selectItem->getAlias();
+			$attrAlias = $selectItem->getSelectItemAlias();
+			$attrName = $selectItem->getAttrName();
+			
+			if (in_array($attrName, $geoAttrsOfQuery[$alias])) {
+
+				$geoAttrAssoc = DatabaseNormalization::simpleAssoc($attrName);
+				$attrMainProjection = new SelectAttribute($alias, $geoAttrAssoc, $attrAlias);
+
+				$processedSelects->mainSelect->add($attrMainProjection);
+				
+				$attrGeo = new stdClass();
+				$attrGeo->alias = ($attrAlias == null)? $attrName : $attrRes->getSelectItemAlias();
+				$attrGeo->attrGeo = $geoAttrAssoc;
+				$attrGeo->object = $alias;
+				$attrGeo->attr = $attrName;
+				
+				$processedSelects->tableAttrGeo[$attrGeo->alias] = $attrGeo;
+				
+				$attrGisProjectionId = new SelectAttribute($alias, 'id');
+				$attrGisProjectionGeom = new SelectAttribute($alias, 'geom', $attrGeo->alias);
+				$processedSelects->gisSelect[] = new Select(array($attrGisProjectionId, $attrGisProjectionGeom));
+				
+			} else {
+				return new SelectAttribute($alias, $selectItem->getAttrName(), $selectItem->getSelectItemAlias()); 
+			}
+			
+		} else if ($selectItem instanceof SelectAggregation) {
+			
+			return new SelectAggregation($selectItem->getName(), 
+				$this->GISProjectionToProjection($geoAttrsOfFroms, $selectItem->getParam(), $tableAttrGeo), 
+				$selectItem->getSelectItemAlias());
+				
+		} else if ($selectItem instanceof SelectValue) {
+			return new SelectValue($selectItem->getValue(), $selectItem->getSelectItemAlias());
+			
+		} else if ($selectItem instanceof GISFunction) {
+			//TODO_GIS, esto generaría cosas del estilo Area(ubicacio_id), estas funciones debe de ir contra la otra base
+			$params = array();
+			foreach ($selectItem->getParams() as $param) {
+				$params[] = $this->GISProjectionToProjection($geoAttrsOfFroms, $param, $tableAttrGeo);
+			}
+			return new GISFunction($selectItem->getType(), $params, $selectItem->getSelectItemAlias());
+			
+		} else {
+			throw new Exception("No implementado");
+		}
 	}
 	
 	/**
@@ -46,17 +111,52 @@ class GISQueryProcessor {
 	/**
 	 * Se encarga de ejececutar las querys sobre la base de datos geografica
 	 */
-	private static function executeGISQuerys() {
+	private static function createGISQuerys() {
 		//TODO_GIS
 	}
 	
 	/**
 	 * Se arma el resultado
 	 */
-	private static function processResult() {
+	private static function processResults() {
 		//TODO_GIS
 	}
 	
+	
+	//// Metodos auxiliares
+	
+	/**
+	 * Retorna un array con los atributos geograficos de tablas en un From
+	 * @param array $from
+	 */
+	private static function getGeometryAttrs(array $from) {
+		$geoAttrsOfFroms = array();
+		foreach ($froms as $from) {
+			// TODO_GIS ver de mejorar el caso de que se realize From de las mismas tablas y no se realize
+			// dos veces la busqueda
+			$ins = $from->instance_or_class;
+			if ( !is_object($ins) ) {
+				$ins = new $instance_or_class(array(), true);
+			}
+			$geoAttrsOfFroms[$from->alias] = $ins->hasGeometryAttributes();
+		}
+		return $geoAttrsOfFroms;
+	}
+	
+}
+
+//// Clases auxiliares
+
+class ProcessedSelects {
+	public $mainSelect;
+	public $gisSelect;
+	public $tableAttrGeo;
+	
+	public function __construct() {
+		$this->mainSelect = new Select();
+		$this->gisSelect = array();
+		$this->tableAttrGeo = array();
+	}
 }
 
 
