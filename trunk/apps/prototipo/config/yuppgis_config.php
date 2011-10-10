5a305a9f568f391338499509f64922de8be32d6b
jdavisonc <jdavisonc@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-10 14:41:16 +0000

    Cambio de premium a basico

--- trunk/apps/prototipo/config/yuppgis_config.php
+++ trunk/apps/prototipo/config/yuppgis_config.php
@@ -2,6 +2,7 @@
 
 $srid = 32721;
 $google_maps_key = 'ABQIAAAA9a4X6TFheB81m4gfqmoVHRQl3YGh390M_4O6wxlWQ-a6-Oz5rBRtOqOhlPbudwJAoZZBg_gql7zLpg';
+//$google_maps_key = 'ABQIAAAA9a4X6TFheB81m4gfqmoVHRTB4vxG9QIfIaQXdPTsELnQKJj6tRQ_bsOGsHKhljG_DftJC5pb06upWA';
 
 $gisdb = array( YuppConfig::MODE_DEV  => array(
                                      'type'     => YuppConfig::DB_POSTGRES,
@@ -12,6 +13,6 @@ $gisdb = array( YuppConfig::MODE_DEV  => array(
                                    )
 );
 
-$yuppgis_mode = YuppGISConfig::MODE_BASIC;
+$yuppgis_mode = YuppGISConfig::MODE_PREMIUM;
 
 ?>
