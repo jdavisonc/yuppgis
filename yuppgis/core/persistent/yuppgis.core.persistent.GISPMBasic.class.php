db595409d4f336f534ac14b3f4d8d731912391d0
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-12 23:32:37 +0000

    Agrego KMLGeo.

--- yuppgis/core/persistent/yuppgis.core.persistent.GISPMBasic.class.php
+++ yuppgis/core/persistent/yuppgis.core.persistent.GISPMBasic.class.php
@@ -9,28 +9,29 @@ class GISPMBasic extends GISPersistentManager {
 		// TODO_GIS: Deberia de tener dos instancias de dal distintas? uno comun y otro wsdal? xq esta clase no es igual que premium, 
 		//			 ya q uno es ws y otro db.
 		$this->dal = new DAL($appName);
-		$this->giswsdal = new RestGISWSDAL(); 
+		$this->giswsdal = new RestGISWSDAL($appName); 
 	}
 
 	/**
 	 * @see GISPersistentManager::get_gis_object()
 	 */
 	public function get_gis_object( $tableNameOwner, $attr, $persistentClass, $id ) {
-		$kml = $this->giswsdal->get();
+		$kml = $this->giswsdal->get($tableNameOwner, $attr, $persistentClass, $id);
+		
 	}
 
 	/**
 	 * @see GISPersistentManager::save_gis_object()
 	 */
 	protected function save_gis_object( $ownerTableName, $attrNameAssoc, PersistentObject $obj ) {
-		$kml = $this->giswsdal->save();
+		$this->giswsdal->save();
 	}
 
 	/**
 	 * @see GISPersistentManager::delete_gis_object()
 	 */
 	protected function delete_gis_object($owner, $attrNameAssoc, $assocObj, $logical) {
-		$kml = $this->giswsdal->delete();
+		$this->giswsdal->delete();
 	}
 
 	/**
