352b52573dc44da907be0453a96c828d9a5c3766
emiliarosag@gmail.com <emiliarosag@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-15 17:57:09 +0000

    Van cambios en el color Cambios en kml

--- yuppgis/core/gis/yuppgis.core.gis.KMLUtilities.class.php
+++ yuppgis/core/gis/yuppgis.core.gis.KMLUtilities.class.php
@@ -31,7 +31,7 @@ class KMLUtilities{
 
 		return $kml;
 	}
-	
+
 	private static function ElementToKml($element, $layer){
 
 		$kml = '';
@@ -43,21 +43,21 @@ class KMLUtilities{
 			<className>'.get_class($element).'</className>
 			<layerId>'.$layer->getId().'</layerId>
 			<elementId>'.$element->getId().'</elementId>
-			<gisType>'.GISDatatypes::POINT.'</gisType>
-			<Style>
-		        <IconStyle>
-		          <scale>0.8</scale>
-		          <Icon>
-		            <href>'.$layer->getIconurl().'</href>
-		          </Icon>
-		        </IconStyle>
-		    </Style>		    
-			<Point>
+			<gisType>'.GISDatatypes::POINT.'</gisType>';
+
+			if($element->getUbicacion()->hasAttribute('uiproperty')){
+				$kml.= KMLUtilities::UIPropertyToKml(GISDatatypes::POINT, $element->getUbicacion()->getUIProperty(), $layer);
+					
+
+			}
+				
+
+			$kml .=	'<Point>
 				<coordinates>'.$element->getUbicacion()->getX().','.$element->getUbicacion()->getY().'</coordinates>
 			</Point>
 		</Placemark>';
 		}
-		
+
 		if ($element->hasAttribute('linea')){
 
 			$kml .= '
@@ -68,24 +68,24 @@ class KMLUtilities{
 			<layerId>'.$layer->getId().'</layerId>
 			<elementId>'.$element->getId().'</elementId>
 			<gisType>'.GISDatatypes::LINESTRING.'</gisType>';						
-			if($element->getLinea() != null){				
+			if($element->getLinea() != null){
 				if($element->getLinea()->hasAttribute('uiproperty')){
-						$kml.= KMLUtilities::UIPropertyToKml(GISDatatypes::LINESTRING, $element->getLinea()->getUIProperty());												
-				}				
-			$kml.=	'<LineString>
+					$kml.= KMLUtilities::UIPropertyToKml(GISDatatypes::LINESTRING, $element->getLinea()->getUIProperty(), $layer);
+				}
+				$kml.=	'<LineString>
 				<coordinates>';			
 				foreach ($element->getLinea()->getPoints() as $point){
 					$kml.=	$point->getX().','.$point->getY().',0. ';
-				}			
-			$kml.=	'</coordinates>
+				}
+				$kml.=	'</coordinates>
 			</LineString>';
 			}
-		$kml .= '</Placemark>';
+			$kml .= '</Placemark>';
 		}
-		
+
 		if ($element->hasAttribute('zonas')){
-				foreach ($element->getZonas()->getCollection() as $zona) {
-					$kml .= '
+			foreach ($element->getZonas()->getCollection() as $zona) {
+				$kml .= '
 						<Placemark>
 							<name>'.$element->getId().'</name>
 							<description>Capa: '.$layer->getName().', Id: '.$element->getId()	.'</description>
@@ -93,91 +93,130 @@ class KMLUtilities{
 							<layerId>'.$layer->getId().'</layerId>
 							<elementId>'.$element->getId().'</elementId>
 							<gisType>'.GISDatatypes::POLYGON.'</gisType>';					
-							if($element->getZonas()->hasAttribute('uiproperty')){
-								$kml.= KMLUtilities::UIPropertyToKml(GISDatatypes::POLYGON, $element->getZonas()->getUIProperty());																
-							}		
-					$kml .= '<Polygon><outerBoundaryIs>
+				if($element->getZonas()->hasAttribute('uiproperty')){
+					$kml.= KMLUtilities::UIPropertyToKml(GISDatatypes::POLYGON, $element->getZonas()->getUIProperty(), $layer);
+				}
+				$kml .= '<Polygon>
+								<outerBoundaryIs>
         						<LinearRing>
         						<coordinates>';			
-									foreach ($zona->getExteriorBoundary()->getPoints() as $point){
-										$kml.=	$point->getX().','.$point->getY().',0. ';
-									}			
-						$kml.=	'</coordinates>
+				foreach ($zona->getExteriorBoundary()->getPoints() as $point){
+					$kml.=	$point->getX().','.$point->getY().',0. ';
+				}
+				$kml.=	'</coordinates>
 								 </LinearRing>
-							</outerBoundaryIs>';
-         			
-					if ($zona->getInteriorsBoundary() != null) {
-						
-						$kml .= '<innerBoundaryIs>';
-						foreach ($zona->getInteriorsBoundary() as $line) {
-							$kml.=	'<LineRing>
+								</outerBoundaryIs>';
+
+				if ($zona->getInteriorsBoundary() != null) {
+
+					$kml .= '<innerBoundaryIs>';
+					foreach ($zona->getInteriorsBoundary() as $line) {
+						$kml.=	'<LineRing>
 									<coordinates>';			
-										foreach ($line->getPoints() as $point){
-											$kml.=	$point->getX().','.$point->getY().',0. ';
-										}			
-							$kml.=	'</coordinates>
+						foreach ($line->getPoints() as $point){
+							$kml.=	$point->getX().','.$point->getY().',0. ';
+						}
+						$kml.=	'</coordinates>
 									</LineRing>
 									</innerBoundaryIs>';
-						}
-						
 					}
-					$kml .= '</Polygon></Placemark>';
+
 				}
+				$kml .= '</Polygon></Placemark>';
+			}
 		}
 
 		return $kml;
 	}
-	
-	
-	private static function UIPropertyToKml($gisDatatypes, $uip)
-	{	
+
+	private static function DefaultStyleForLayer($gisDatatypes, $layer)
+	{
+		switch ($gisDatatypes){
+			case GISDatatypes::POINT:
+				return '<Style>
+					        <IconStyle>
+					          <scale>0.8</scale>
+					          <Icon>
+					             <href>'.$layer->getIconurl().'</href>
+					          </Icon>
+					        </IconStyle>
+					    </Style>';		       			
+				break;
+			//TODO Preguntar por los valores por defecto de la capa
+			case GISDatatypes::LINERING:
+			case GISDatatypes::LINESTRING:				
+			case GISDatatypes::POLYGON:
+			default:
+				return '<Style />';
+				break;
+		}
+
+	}
+
+	private static function UIPropertyToKml($gisDatatypes, $uip, $layer)
+	{
 		switch (get_class($uip)) {
-		    case "Background":{
-		       	switch ($gisDatatypes){
-		       		case GISDatatypes::POLYGON:
-		       			return '<Style>
+			case "Icon":{
+				switch ($gisDatatypes){
+					case GISDatatypes::POINT:
+						return '<Style>
+							        <IconStyle>
+							          <scale>0.8</scale>
+							          <Icon>
+							            <href>'.$uip->getIcon().'</href>
+							          </Icon>
+							        </IconStyle>
+							    </Style>';		       			
+						break;
+					case GISDatatypes::LINERING:
+					case GISDatatypes::LINESTRING:
+					case GISDatatypes::POLYGON:
+					default:
+						return '<Style />';
+						break;
+				}
+				break;
+			}
+			case "Background":{
+				switch ($gisDatatypes){
+					case GISDatatypes::POLYGON:
+						return '<Style>
 						    		<PolyStyle>									    		 
-						      			<color>'.Color::getColorName($uip->getColor()).'</color>									      
+						      			<color>'.$uip->getColor().'</color>									      
 						    		</PolyStyle>
 				  				</Style>';		       			
-		       			break;
-	       			case GISDatatypes::LINESTRING:
-	       				return '';		       			
-	       				break;
-	       			case GISDatatypes::POINT:
-	       				return '';		       			
-	       				break;
-		       		default:
-		       			return '';
-		       			break;	
-		       	}
-		        break;
-		    }		        
-		    case "Border":{
-		    	switch ($gisDatatypes){
-		    		case GISDatatypes::POLYGON:
-	       					return '';		       			
-	       					break;
-			    	case GISDatatypes::LINESTRING:
-			       			return '<Style>
-							    		<LineStyle>									    		 
-							      		<color>'.Color::getColorName($uip->getColor()).'</color>							      		
+						break;
+					case GISDatatypes::LINERING:
+					case GISDatatypes::LINESTRING:
+					case GISDatatypes::POINT:
+					default:
+						return '<Style />';
+						break;
+				}
+				break;
+			}
+			case "Border":{
+				switch ($gisDatatypes){
+					case GISDatatypes::POLYGON:
+					case GISDatatypes::LINERING:
+					case GISDatatypes::LINESTRING:
+						return '<Style>
+						    		<LineStyle>									    		 
+							      		<color>'.$uip->getColor().'</color>							      		
 							      		<width>'.$uip->getWidth().'</width>								      
-							    		</LineStyle>
-						  			</Style>';		       			
-			       			break;			       	
-	       			case GISDatatypes::POINT:
-	       					return '';		       			
-	       					break;
-			    	default:
-			    			return '';
-			           		break;
-		    	}
-		    	break;
-		    }
-		    default:
-		    	return '';
-		    	break; 
+						    		</LineStyle>
+					  			</Style>';		       			
+						break;							
+					case GISDatatypes::POINT:
+					default:
+						return '<Style />';
+						break;
+				}
+				break;
+			}
+			default:
+				return KMLUtilities::DefaultStyleForLayer($gisDatatypes, $layer);
+				break;
 		}
 	}
 }
