6c2fc3d3c0c468c22731ec4d0b5b3e2f85833fd4
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-15 20:44:18 +0000

    Agrego funcion getTypeOf en gisdatatypes.

--- yuppgis/core/db/yuppgis.core.db.GISDatatypes.class.php
+++ yuppgis/core/db/yuppgis.core.db.GISDatatypes.class.php
@@ -15,6 +15,39 @@ class GISDatatypes {
 	const MULTISURFACE = "yuppgis_type_multisurface";
 	const MULTIPOLYGON = "yuppgis_type_multipolygon";
 	const GEOMETRYCOLLECTION = "yuppgis_type_geometrycollection";
+	
+	public static function getTypeOf(Geometry $geom) {
+		switch (get_class($geom)) {
+			case Point::getClassName():
+				return self::POINT;
+			case LineString::getClassName():
+				return self::LINESTRING;
+			case LineRing::getClassName():
+				return self::LINERING;
+			case Line::getClassName():
+				return self::LINE;
+			case Surface::getClassName():
+				return self::SURFACE;
+			case Polygon::getClassName():
+				return self::POLYGON;
+			case Curve::getClassName():
+				return self::CURVE;
+			case MultiPoint::getClassName():
+				return self::MULTIPOINT;
+			case MultiCurve::getClassName():
+				return self::MULTICURVE;
+			case MultiLineString::getClassName():
+				return self::MULTILINESTRING;
+			case MultiSurface::getClassName():
+				return self::MULTISURFACE;
+			case MultiPolygon::getClassName():
+				return self::MULTIPOLYGON;
+			case GeometryCollection::getClassName():
+				return self::GEOMETRYCOLLECTION;
+			default:
+				throw new Exception('Unsupported Geometry');
+		}
+	}
 
 }
 
