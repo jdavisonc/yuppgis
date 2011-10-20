75db08958e60f2b0e4034913bfdff4ac9e5b60be
mtincho12@gmail.com <mtincho12@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-20 21:06:02 +0000

    Subo nuevo app

---
+++ apps/prototipo2DB/controllers/apps.prototipo2DB.controllers.BasicHomeController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+YuppLoader::load('yuppgis.core.mvc', 'GISController');
+
+class HomeController extends GISController {
+
+	public function indexAction()
+	{
+		return $this->redirect(array ("action" => "map"));
+	}
+
+	public function mapAction(){
+		return;
+	}
+	
+}
+
+?>
