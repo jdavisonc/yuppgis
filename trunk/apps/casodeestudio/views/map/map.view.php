ce9d2af3acc68faebdbf3c41e23be924595b59ed
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-29 19:50:10 +0000

    Agrego css de Twitter Bootstrap para caso de estudio.

--- trunk/apps/casodeestudio/views/map/map.view.php
+++ trunk/apps/casodeestudio/views/map/map.view.php
@@ -2,20 +2,69 @@
 <?php echo Helpers::css(array('app'=>'casodeestudio', 'name' => 'main')) ; ?>
 
 <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
 
-<h1>Salud Digital</h1>
 
-  <div class="container-fluid">
-    <div class="sidebar">
-      Test
+    <div class="topbar">
+      <div class="topbar-inner">
+        <div class="container-fluid">
+          <a class="brand" href="#">Salud Digital</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <p class="pull-right">Logged in as <a href="#">username</a></p>
+        </div>
+      </div>
     </div>
-    <div class="content">
-		<?php echo GISHelpers::Map(array(
-			MapParams::ID => 1,
-			MapParams::HEIGHT => 500,
-			MapParams::WIDTH => 500,
-			MapParams::TYPE => "google"
-			
-		)); ?>
+
+    <div class="container-fluid">
+      <div class="sidebar">
+        <div class="well">
+          <h5>Sidebar</h5>
+          <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+          <h5>Sidebar</h5>
+          <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+          <h5>Sidebar</h5>
+          <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="content">
+        <!-- Main hero unit for a primary marketing message or call to action -->
+        <div class="hero-unit">
+          <p>
+          	<?php echo GISHelpers::Map(array(
+				MapParams::ID => 1,
+				MapParams::HEIGHT => 400,
+				MapParams::WIDTH => 850,
+				MapParams::TYPE => "google"
+				
+			)); ?>
+          </p>
+        </div>
+
+        <footer>
+          <p>&copy; YuppGIS 2011</p>
+        </footer>
+      </div>
     </div>
-  </div>
