88ed4386c87fb43391e82935a1493c241fc59ee7
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-10-29 23:50:08 +0000

    Cambio views.

--- trunk/apps/casodeestudio/views/map/map.view.php
+++ trunk/apps/casodeestudio/views/map/map.view.php
@@ -38,20 +38,6 @@ body {
             <li><a href="#">Link</a></li>
             <li><a href="#">Link</a></li>
           </ul>
-          <h5>Sidebar</h5>
-          <ul>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-          </ul>
-          <h5>Sidebar</h5>
-          <ul>
-            <li><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-          </ul>
         </div>
       </div>
       <div class="content">
