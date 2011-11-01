544c1f7011914afbfef39f48895a209ad9d4f657
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-11-01 00:00:38 +0000

    Agrego para editar enfermedades e informacion de paciente.

--- apps/casodeestudio/views/paciente/info.view.php
+++ apps/casodeestudio/views/paciente/info.view.php
@@ -66,14 +66,77 @@ body {
       </div>
       <div class="content">
 			  <div class="page-header">
-			    <h3>Paciente</h3>
+			    <h1><?php echo $paciente->getNombre() . ' ' . $paciente->getApellido(); ?></h1>
 			  </div>
 			  <div class="row">
 			    <div class="span16">
-			    	<div class="row show-grid">
-					    <div class="span-one-third">Nombre</div>
-						<div class="span-two-thirds"><?php echo $paciente->getNombre(); ?></div>
+			    	<div class="row grid">
+			    		<div class="span3 title ">Cedula de Identidad</div>
+						<div class="span3  value"><?php echo $paciente->getCi(); ?></div>
+						<div class="span1"></div>
+					    <div class="span3 title ">Sexo</div>
+						<div class="span3  value"><?php echo $paciente->getSexo(); ?></div>
 					</div>
+					<div class="row grid">
+					    <div class="span3 title ">F.Nacimiento</div>
+						<div class="span3  value"><?php echo $paciente->getFechaNacimiento(); ?></div>
+						<div class="span1"></div>
+						<div class="span3 title ">F.Fallecimiento</div>
+						<div class="span3  value"><?php echo $paciente->getFechaFallecimiento(); ?></div>
+					</div>
+					<div class="row grid">
+					    <div class="span3 title ">Telefono</div>
+						<div class="span3  value"><?php echo $paciente->getTelefono(); ?></div>
+						<div class="span1"></div>
+						<div class="span3 title ">Email</div>
+						<div class="span3  value"><?php echo $paciente->getEmail(); ?></div>
+					</div><br/>
+					
+					<div class="page-header">
+					    <h4>Ubicacion</h4>
+					  </div>
+					<div class="row grid">
+					    <div class="span3 title ">Direccion</div>
+						<div class="span3  value"><?php echo $paciente->getDireccion(); ?></div>
+						<div class="span1"></div>
+						<div class="span3 title ">Barrio</div>
+						<div class="span3  value"><?php echo $paciente->getBarrio(); ?></div>
+					</div>
+					<div class="row grid">
+					    <div class="span3 title">Ciudad</div>
+						<div class="span3 value"><?php echo $paciente->getCiudad(); ?></div>
+						<div class="span1" ></div>
+						<div class="span3 title">Departamento</div>
+						<div class="span3 value"><?php echo $paciente->getDepartamento(); ?></div>
+					</div><br/>
+					<div class="page-header">
+					   <h4>Enfermedades</h4>
+					</div>
+					<div class="row grid">
+					    <div class="span3 title "><?php echo Enfermedad::getName(Enfermedad::ASMA); ?></div>
+						<div class="span3  value"><?php echo $paciente->aGet(Enfermedad::ASMA); ?></div>
+						<div class="span1" ></div>
+						<div class="span3 title "><?php echo Enfermedad::getName(Enfermedad::DIABETES); ?></div>
+						<div class="span3  value"><?php echo $paciente->aGet(Enfermedad::DIABETES); ?></div>
+					</div>
+					<div class="row grid">
+					    <div class="span3 title "><?php echo Enfermedad::getName(Enfermedad::HIPERTENCION); ?></div>
+						<div class="span3  value"><?php echo $paciente->aGet(Enfermedad::HIPERTENCION); ?></div>
+						<div class="span1" ></div>
+						<div class="span3 title "><?php echo Enfermedad::getName(Enfermedad::INSUFICIENCIA_RENAL); ?></div>
+						<div class="span3  value"><?php echo $paciente->aGet(Enfermedad::INSUFICIENCIA_RENAL); ?></div>
+					</div>
+					<div class="row grid">
+					    <div class="span3 title "><?php echo Enfermedad::getName(Enfermedad::OBESIDAD); ?></div>
+						<div class="span3  value"><?php echo $paciente->aGet(Enfermedad::OBESIDAD); ?></div>
+					</div><br/>
+					<?php echo Helpers::link( array(
+							"app"        => "casodeestudio",
+                            "controller" => "paciente",
+                            "action"     => "editEnf",
+                            "body"       => "Editar Enfermedades",
+							"id"         => $paciente->getId(),  
+			    			"attrs"		 => array ("class" => "btn primary")) ); ?>
 		        </div>
 		     </div>
 		  
