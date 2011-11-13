3f18d071ece7a21713e0349af12ae79bff59408e
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2011-11-13 19:38:36 +0000

    Agrego sql para insercion de datos para aplicacion geolocalizacion.

--- trunk/apps/geolocalizacion/bootstrap/data_transform.sql
+++ trunk/apps/geolocalizacion/bootstrap/data_transform.sql
@@ -1,3 +1,6 @@
+-- Se necesita Base de Datos de numero padrones (v_mdg_accesos) de la Intendencia de Municipal de Montevideo (Uruguay)
+-- Se puede descargar de forma gratuita desde la pagina: http://sig.montevideo.gub.uy/
+
 truncate padron;
 truncate padron_ubicacion_geo;
 insert into padron (id, calle, numero, padron, ubicacion_id, "class", deleted) select gid, nom_calle, num_puerta, padron, gid, 'Padron', false from v_mdg_accesos;
