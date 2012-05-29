da277e1f2ed288b073558b5680504b382121eb37
jdavisonc@gmail.com <jdavisonc@gmail.com@0b253e03-9307-0b4d-d31f-21791feacc79>
2012-05-29 03:00:41 +0000

    Se mueve applicacion a nuevo directorio.

--- trunk/apps/geolocalizacion/views/geo/index.view.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h1>Geolocalizacion</h1>
-</body>
-</html>
--- trunk/apps/prototipo/views/home/PPaciente.Sanos.template.php
+++
@@ -1,6 +0,0 @@
-<div>
-  Estoy en template de capa: <?php echo $layer->getName(); ?>
-</div>
-
- <?php  $element = PPaciente::get($elementId); ?>
- Estoy viendo a <?php echo $element->getNombre(); ?> !!
